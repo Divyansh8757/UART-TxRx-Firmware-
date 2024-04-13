25626cab7acb6a4c7f15177ccf4f0abd189fc5fb
Divyansh8757 <166917600+Divyansh8757@users.noreply.github.com>
2024-04-13 17:50:04 +0530

    Update Important Peice of MC Code.c

--- Core/Src/Important Peice of MC Code.c
+++ Core/Src/Important Peice of MC Code.c
@@ -20,14 +20,14 @@ void Start_Transmission();
 void Receive_TxRequest();
 
 uint8_t rxData[1010];	    	// Data Receive Buffer
-uint8_t rxReq[1];			      // Data Transmission request from PC
-uint8_t txReq[1];		      	// Data Reception request from PC
-uint8_t res[1]={'1'};		    // Positive response from MC to PC on request
-int i=0;					          // counter for data reception buffer
-int j=0;					          // counter for data transmission buffer
-int size=0;					        // To check size of data received and transmitted
-int rate=0;					        // to calculate rate of data reception and transmission
-char Rate[36];				      // Character buffer to print reception and transmission rate
+uint8_t rxReq[1];		// Data Transmission request from PC
+uint8_t txReq[1];		// Data Reception request from PC
+uint8_t res[1]={'1'};		// Positive response from MC to PC on request
+int i=0;			// counter for data reception buffer
+int j=0;			// counter for data transmission buffer
+int size=0;			// To check size of data received and transmitted
+int rate=0;			// to calculate rate of data reception and transmission
+char Rate[36];			// Character buffer to print reception and transmission rate
 
 int main(void)
 {
@@ -45,16 +45,16 @@ int main(void)
 
 HAL_TIM_Base_Start(&htim4);
 
- while (1)
+  while (1)
   {
 	  i=0;
 	  j=0;
-	  HAL_UART_Receive(&huart1, rxReq, 1,HAL_MAX_DELAY);  // Wait for transmission request from PC
-	  if(rxReq[0]=='1'){					 // If request received
-	  	Send_Response();								                  // Send Acknowledgement response
-	  	Receive_Data();								                	  // Function to receive data from PC to MC
-	  } 
-	  Start_Transmission();								                // Funtion To transmit data from MC to PC
+	  HAL_UART_Receive(&huart1, rxReq, 1,HAL_MAX_DELAY);	// Wait for transmission request from PC
+	  if(rxReq[0]=='1'){					// If request received
+	  	Send_Response();				// Send Acknowledgement response
+	  	Receive_Data();					// Function to receive data from PC to MC
+	  }
+	  Start_Transmission();					// Funtion To transmit data from MC to PC
   }
 }
 
